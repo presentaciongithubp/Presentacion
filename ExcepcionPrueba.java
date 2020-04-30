2054a0a20222f48055f43d929b5fb17858c7c4e6
presentaciongithub <64598517+presentaciongithub@users.noreply.github.com>
2020-04-30 16:51:30 +0200

    ExcepcionPrueba
    Prueba

--- ExcepcionPrueba.java
+++ ExcepcionPrueba.java
@@ -1,11 +1,10 @@
 package gestionPersonasAplicacion;
 
 @SuppressWarnings("serial")
-
-public class ExcepcionPrueba extends RuntimeException{
+public class ExcepcionPrueba extends RuntimeException {
 
 	public ExcepcionPrueba(String mensage) {
-			super("Error 404: "+mensage);
+		super("Fallo: " + mensage);
 	}
-	
+
 }
